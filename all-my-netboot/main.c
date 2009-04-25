6542b88540ec18605d6e3a5f566c8dd6561eb2cf
Dwayne C. Litzenberger <dlitz@dlitz.net>
2009-04-25 18:27:44 -0400

    Add code to create a new gdt

--- all-my-netboot/main.c
+++ all-my-netboot/main.c
@@ -42,14 +42,12 @@ static void dump_segment_descriptor(struct segdesc *desc)
     l_print("\r\n", 0);
 }
 
-static void dump_gdt(void)
+static void dump_gdt(void *base, uint16_t limit)
 {
-    struct gdtr gdtr;
-    get_gdtr(&gdtr);
-    l_print("GDT located at base: 0x%08x", (uint32_t) gdtr.base);
-    l_print(" limit: 0x%04x\r\n", gdtr.limit);
-    struct segdesc *p = (struct segdesc *)gdtr.base;
-    for (int i = 0; i <= gdtr.limit; i += 8) {
+    l_print("GDT located at base: 0x%08x", (uint32_t) base);
+    l_print(" limit: 0x%04x\r\n", limit);
+    struct segdesc *p = (struct segdesc *)base;
+    for (int i = 0; i <= limit; i += 8) {
         dump_segment_descriptor(p);
         p++;
     }
@@ -71,6 +69,72 @@ static void dump_regs(void)
     l_print("CR4: 0x%08x\r\n", get_cr4_reg());
 }
 
+static void bzero(void *s, unsigned int n)
+{
+    unsigned char *p = (unsigned char *)s;
+    while (n > 0) {
+        *p = '\0';
+        p++;
+        n--;
+    }
+}
+
+struct segdesc real_mode_gdt[5];
+
+static void set_desc_base(struct segdesc *desc, void *base)
+{
+    uint32_t addr = (uint32_t)base;
+    desc->base0 = addr & 0xffff;
+    desc->base1 = (addr >> 16) & 0xff;
+    desc->base2 = (addr >> 24) & 0xff;
+}
+
+static void set_desc_limit(struct segdesc *desc, uint32_t limit)
+{
+    desc->limit0 = limit & 0xffff;
+    desc->limit1 = (limit >> 16) & 0xf;
+}
+
+void create_real_mode_gdt(struct segdesc *gdt)
+{
+    bzero(gdt, sizeof(struct segdesc)*5);
+    set_desc_base(&gdt[1], 0x00000000);
+    set_desc_limit(&gdt[1], 0xfffff);
+    gdt[1].db = 1;
+    gdt[1].dpl = 0;
+    gdt[1].g = 1;
+    gdt[1].p = 1;
+    gdt[1].s = 1;
+    gdt[1].type = 0xb; /* code */
+
+    set_desc_base(&gdt[2], 0x00000000);
+    set_desc_limit(&gdt[2], 0xfffff);
+    gdt[2].db = 1;
+    gdt[2].dpl = 0;
+    gdt[2].g = 1;
+    gdt[2].p = 1;
+    gdt[2].s = 1;
+    gdt[2].type = 0x3; /* data */
+
+    set_desc_base(&gdt[3], 0x00000000);
+    set_desc_limit(&gdt[3], 0x0ffff);
+    gdt[3].db = 0;      /* 16-bit segment */
+    gdt[3].dpl = 0;
+    gdt[3].g = 0;   /* byte granularity */
+    gdt[3].p = 1;
+    gdt[3].s = 1;
+    gdt[3].type = 0xb; /* code */
+
+    set_desc_base(&gdt[4], 0x00000000);
+    set_desc_limit(&gdt[4], 0x0ffff);
+    gdt[4].db = 0;      /* 16-bit segment */
+    gdt[4].dpl = 0;
+    gdt[4].g = 0;   /* byte granularity */
+    gdt[4].p = 1;
+    gdt[4].s = 1;
+    gdt[4].type = 0x3; /* data */
+}
+
 void init_c(void)
 {
     int x = 1;
@@ -78,5 +142,12 @@ void init_c(void)
     l_print("stack is in the ballpark of 0x%x\r\n", (uint32_t) &x);
     l_print("test_return returned 0x%x\r\n", test_return(5));
     dump_regs();
-    dump_gdt();
+
+    struct gdtr gdtr;
+    get_gdtr(&gdtr);
+    dump_gdt(gdtr.base, gdtr.limit);
+
+    l_print("Creating real-mode GDT\r\n", 0);
+    create_real_mode_gdt(real_mode_gdt);
+    dump_gdt(real_mode_gdt, 5*8-1);
 }
