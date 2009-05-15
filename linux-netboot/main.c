837d2760777bb7712d1421d441c88881a0dac589
Dwayne C. Litzenberger <dlitz@dlitz.net>
2009-05-15 01:08:34 -0400

    Create pirq.c

--- linux-netboot/main.c
+++ linux-netboot/main.c
@@ -2,42 +2,11 @@
 #include <stdint.h>
 #include <stdbool.h>
 #include "portio.h"
+#include "pirq.h"
 #include "mynetboot.h"
 
 extern void *bzImage_start;
 extern void load_linux(void);
-
-#define PIRQ_NUM_SLOTS 7
-
-struct pirq_slot {
-    uint8_t pci_bus;
-    uint8_t pci_devfunc;
-    uint8_t inta_link;
-    uint16_t inta_bitmap;
-    uint8_t intb_link;
-    uint16_t intb_bitmap;
-    uint8_t intc_link;
-    uint16_t intc_bitmap;
-    uint8_t intd_link;
-    uint16_t intd_bitmap;
-    uint8_t slot_number;
-    uint8_t _reserved[1];
-} __attribute__((packed));
-
-struct pirq_table {
-    uint32_t signature;
-    uint16_t version;
-    uint16_t table_size;
-    uint8_t irq_router_bus;
-    uint8_t irq_router_devfunc;
-    uint16_t exclusive_irq;
-    uint16_t irq_router_compat_vendor_id;
-    uint16_t irq_router_compat_device_id;
-    uint32_t miniport_data;
-    uint8_t _reserved[11];
-    uint8_t checksum;   /* mod 256 must equal 0 */
-    struct pirq_slot slots[PIRQ_NUM_SLOTS];
-} __attribute__((packed));
 
 /* Invoke the loader's printf-like function */
 void l_print(const char *fmt, uint32_t arg)
@@ -283,64 +252,6 @@ void dump_flash(void)
     //}
         i++;
     } while (p != 0);
-}
-
-// See http://www.microsoft.com/whdc/archive/pciirq.mspx
-// PCI IRQ Routing Table Specification
-// Microsoft Corporation, Version 1.0, February 27, 1998
-// (Updated: December 4, 2001)
-extern void create_pirq_table(void)
-{
-    struct pirq_table *t = (struct pirq_table *)0xf0000;
-    bzero(t, sizeof(struct pirq_table));
-    t->signature = '$' | ('P' << 8) | ('I' << 16) | ('R' << 24);
-    t->version = 0x0100;
-    t->table_size = 32 + PIRQ_NUM_SLOTS*16;
-    t->irq_router_bus = 0;
-    t->irq_router_devfunc = 0x90; // CS5530A: F0 Bridge Configuration
-    t->exclusive_irq = 0;  // No IRQs are exclusive to PCI (XXX: probably wrong)
-    t->irq_router_compat_vendor_id = 0x1078;    // PCI_VENDOR_ID_CYRIX
-    t->irq_router_compat_device_id = 0x0002;    // PCI_DEVICE_ID_CYRIX_5520
-    t->miniport_data = 0;
-
-    for (unsigned int i = 0; i < PIRQ_NUM_SLOTS; i++) {
-        bzero(&t->slots[i], sizeof(struct pirq_slot));
-        t->slots[i].pci_bus = 0;
-        switch (i) {
-        // See the CS5530A documentation,
-        // Table 5-1 "PCI Configuration Address Register"
-        case 0: // Function 0: Bridge Configuration
-            t->slots[i].pci_devfunc = 0x90;
-        case 1: // Function 1: SMI Status and ACPI Timer
-            t->slots[i].pci_devfunc = 0x91;
-        case 2: // Function 2: IDE Controller
-            t->slots[i].pci_devfunc = 0x92;
-        case 3: // Function 3: XpressAUDIO Subsystem
-            t->slots[i].pci_devfunc = 0x93;
-        case 4: // Function 4: Video Controller
-            t->slots[i].pci_devfunc = 0x94;
-        case 5: // USB Controller
-            t->slots[i].pci_devfunc = 0x98;
-        case 6: // TI PCI1410APGE CardBus controller
-            t->slots[6].pci_devfunc = 0x0e << 3;
-        }
-        t->slots[i].inta_link = 2;
-        t->slots[i].inta_bitmap = 0xDEFA; // IRQs 1,3,4,5,6,7,9,10,11,12,14,15
-        t->slots[i].intb_link = 1;
-        t->slots[i].intb_bitmap = 0xDEFA; // IRQs 1,3,4,5,6,7,9,10,11,12,14,15
-        t->slots[i].intc_link = 4;
-        t->slots[i].intc_bitmap = 0xDEFA; // IRQs 1,3,4,5,6,7,9,10,11,12,14,15
-        t->slots[i].intd_link = 3;
-        t->slots[i].intd_bitmap = 0xDEFA; // IRQs 1,3,4,5,6,7,9,10,11,12,14,15
-        t->slots[i].slot_number = 0;
-    }
-
-    t->checksum = 0;
-    uint8_t checksum = 0;
-    for (int i = 0; i < t->table_size; i++) {
-        checksum += ((unsigned char *)t)[i];
-    }
-    t->checksum = -checksum;
 }
 
 void memtest(void)
---
+++ linux-netboot/pirq.h
@@ -0,0 +1,40 @@
+#ifndef PIRQ_H
+#define PIRQ_H
+
+#include <stdint.h>
+
+#define PIRQ_NUM_SLOTS 7
+
+struct pirq_slot {
+    uint8_t pci_bus;
+    uint8_t pci_devfunc;
+    uint8_t inta_link;
+    uint16_t inta_bitmap;
+    uint8_t intb_link;
+    uint16_t intb_bitmap;
+    uint8_t intc_link;
+    uint16_t intc_bitmap;
+    uint8_t intd_link;
+    uint16_t intd_bitmap;
+    uint8_t slot_number;
+    uint8_t _reserved[1];
+} __attribute__((packed));
+
+struct pirq_table {
+    uint32_t signature;
+    uint16_t version;
+    uint16_t table_size;
+    uint8_t irq_router_bus;
+    uint8_t irq_router_devfunc;
+    uint16_t exclusive_irq;
+    uint16_t irq_router_compat_vendor_id;
+    uint16_t irq_router_compat_device_id;
+    uint32_t miniport_data;
+    uint8_t _reserved[11];
+    uint8_t checksum;   /* mod 256 must equal 0 */
+    struct pirq_slot slots[PIRQ_NUM_SLOTS];
+} __attribute__((packed));
+
+extern void create_pirq_table(void);
+
+#endif /* PIRQ_H */
